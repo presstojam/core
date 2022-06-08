de0ef10c8c6f26e2990c05c95e53eabb2b9ad6f5
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-06-08 09:50:17 +0100

    Update ShapeHandler.php

--- app/ShapeHandler.php
+++ app/ShapeHandler.php
@@ -73,11 +73,12 @@ class ShapeHandler
     function setReferences() {
         $fields = [];
         foreach($this->output_shape->fields as $slug=>$cell) {
-            if (get_class($cell->meta_field) == "\PressToJamCore\Cells\IdCell") {
+            if (get_class($cell->meta_field) == "PressToJamCore\Cells\IdCell") {
                 if (!$cell->meta_field->is_parent and !$cell->meta_field->is_primary and $cell->meta_field->reference) {
                     $this->output_shape->addRelationship($slug, $cell);
                     $this->collections[$slug] = $cell->meta_field->reference;
                     $fields[$slug][] = "*summary";
+                    $this->collections[$cell->meta_field->reference->slug] = $cell->meta_field->reference;
                 }
             }
         }
