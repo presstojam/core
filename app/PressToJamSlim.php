3fb914877ad8e7dd2db9b13799f6e1827830ed11
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-07-25 21:06:58 +0100

    Fixing autoload

--- app/PressToJamSlim.php
+++ app/PressToJamSlim.php
@@ -48,27 +48,6 @@ class PressToJamSlim {
     }
 
 
-    function regAutoload($namespace, $base)
-    {
-
-        //register psr-4 autoload
-        spl_autoload_register(function ($class_name) use ($namespace, $base) {
-            $parts = explode("\\", $class_name);
-            $file = $base .  "/";
-            $onamespace = array_shift($parts);
-            if ($onamespace == $namespace) {
-                $file .= implode("/", $parts) . ".php";
-                if (file_exists($file)) {
-                    require_once($file);
-                    return;
-                } else {
-                    echo "Can't find file " . $file;
-                }
-            }
-        });
-    }
-
-
 
     function validateRoute($request, $handler) {
 
---
+++ app/PtjAutoload.php
@@ -0,0 +1,22 @@
+<?php
+namespace PtjAutoload;
+
+
+function regAutoload($namespace, $base) {
+    //register psr-4 autoload
+    spl_autoload_register(function ($class_name) use ($namespace, $base) {
+        $parts = explode("\\", $class_name);
+        $file = $base .  "/";
+        $onamespace = array_shift($parts);
+        if ($onamespace == $namespace) {
+            $file .= implode("/", $parts) . ".php";
+            if (file_exists($file)) {
+                require_once($file);
+                return;
+            } else {
+                echo "Can't find file " . $file;
+            }
+        }
+    });
+}
+
